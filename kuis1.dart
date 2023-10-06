7fdeebcf689a4ef22be3eb8816a7c6be9d7e165d
AgRistan <103366011+AgRistan@users.noreply.github.com>
2023-10-06 13:45:53 +0700

    Add files via upload

---
+++ kuis1.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  String ktapertama = "Halo";
+  String ktakedua = "Dunia";
+
+  print(ktapertama + ktakedua);
+
+  String nmalengkap = "Ini adalah Rosid";
+  String nmadepan = nmalengkap.substring(5,12);
+  print(nmadepan);
+}
